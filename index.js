78ea593f59ba15794dbaaa0db7fb3c0d60e5db3c
Tamara Ujmajuridze <Tauj0001@stud.kea.dk>
2025-08-25 18:36:54 +0200

    ini

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("Tilfædligt tal " + Math.ceil(Math.random() * 100));
+
+const tal = document.querySelector("h1");
+document.querySelector("h1").innerHTML = " " + Math.ceil(Math.random() * 100);
